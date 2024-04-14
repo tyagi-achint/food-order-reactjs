317e3c0f4afa97705c8646d9e5de4fff66041021
tyagi-achint <achinttyagi001@gmail.com>
2024-04-14 21:41:16 +0530

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./Header";
+import Meals from "./Meals";
 
 export default function App() {
   return (
     <>
       <Header />
+      <Meals />
     </>
   );
 }
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,3 +1,24 @@
+import { useEffect, useState } from "react";
+import MealItem from "./MealItem";
+
 export default function Meals() {
-  return <div>Meals</div>;
+  const [loadedMeals, setLoadedMeals] = useState([]);
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch("http://localhost:3000/meals");
+
+      const meals = await response.json();
+      setLoadedMeals(meals);
+    }
+
+    fetchMeals();
+  }, []);
+
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <MealItem key={meal.id} meal={meal} />
+      ))}
+    </ul>
+  );
 }
